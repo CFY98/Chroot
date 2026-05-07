b18654c6abbec22d8009adf92bcaedc604d331f8
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 22:11:50 +0100

    fix: added fragment after moving information div class outside of caption-container div class

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,18 +3,25 @@ import styles from "../css/ProductCard.module.css";
 
 function ProductCard({ src, alt, arialabel, href, itemName, itemPrice }) {
   return (
-    <div className={styles["caption-container"]}>
-      <img src={src} alt={alt} className={`${styles.product} fadein`} />
-      <div className={styles.caption}>
-        <a className={styles.more} aria-label={arialabel} href={href} data-link>
-          More info
-        </a>
+    <>
+      <div className={styles["caption-container"]}>
+        <img src={src} alt={alt} className={`${styles.product} fadein`} />
+        <div className={styles.caption}>
+          <a
+            className={styles.more}
+            aria-label={arialabel}
+            href={href}
+            data-link
+          >
+            More info
+          </a>
+        </div>
       </div>
       <div className={`${styles.information} fadein`}>
         <p className={styles.itemnames}>{itemName}</p>
         <p className={styles.prices}>{itemPrice}</p>
       </div>
-    </div>
+    </>
   );
 }
 
