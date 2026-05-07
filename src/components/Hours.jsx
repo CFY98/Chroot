a70a2c1b0892ff600440b4168767deb0f1325dec
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 16:03:18 +0100

    feat: Hours module css for Hours.jsx

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,9 +1,12 @@
+//IMPORTS
+import styles from "../css/Hours.module.css";
+
 function Hours() {
   return (
-    <div className="hours-container">
-      <div className="opening-container">
-        <h3 className="sub-heading fadein">Opening Hours</h3>
-        <p className="times fadein">
+    <div className={styles["hours-container"]}>
+      <div className={styles["opening-container"]}>
+        <h3 className={`${styles["sub-heading"]} fadein`}></h3>
+        <p className={`${styles.times} fadein`}>
           Mondays - Fridays: 07:00 → 18:00
           <br />
           Saturdays - Sundays: 08:00 → 16:00
