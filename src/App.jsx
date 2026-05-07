3bebb75607735f86997386183098ba354747be31
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 14:45:49 +0100

    chore: moved App.jsx to src directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "/src/pages/Home";
-import Beans from "/src/pages/Beans";
-import NavBar from "./src/components/NavBar";
+import Home from "./pages/Home";
+import Beans from "./pages/Beans";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/beans" element={<Beans />} />
       </Routes>
+      <Footer />
     </>
   );
 }
