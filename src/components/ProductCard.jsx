46e927707653f1c182658216ee87d1ab8a157a9e
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 21:32:58 +0100

    feat: ProductCard css module for ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,18 @@
+//IMPORTS
+import styles from "../css/ProductCard.module.css";
+
 function ProductCard({ src, alt, arialabel, href, itemName, itemPrice }) {
   return (
-    <div className="caption-container">
+    <div className={styles["captions-container"]}>
       <img src={src} alt={alt} className="fadein" />
-      <div className="caption">
-        <a className="more" aria-label={arialabel} href={href} data-link>
+      <div className={styles.caption}>
+        <a className={styles.more} aria-label={arialabel} href={href} data-link>
           {true} More info
         </a>
       </div>
-      <div className="information fadein">
-        <p className="itemnames">{itemName}</p>
-        <p className="prices">{itemPrice}</p>
+      <div className={`${styles.information} fadein`}>
+        <p className={styles.itemnames}>{itemName}</p>
+        <p className={styles.prices}>{itemPrice}</p>
       </div>
     </div>
   );
