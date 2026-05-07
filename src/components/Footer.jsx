7b35106dec673997b32e4dfb27001f9b84d9f3b8
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 14:48:12 +0100

    feat: added Footer component for social media

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="socials">
+      <a href="https://github.com/CFY98/Chroot" className="fa-brands fa-github"></a>
+    </div>
+  );
+}
+
+export default Footer;
