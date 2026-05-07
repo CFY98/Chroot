cc98031918fe2c536baaba9cbdd8a5a9bd9268f1
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 12:50:14 +0100

    chore: moved Beans.jsx to tabs directory

--- src/pages/tabs/Beans.jsx
+++ src/pages/tabs/Beans.jsx
@@ -1,7 +1,7 @@
 import "../css/pages.css";
-import Title from "./Title";
-import Subtitle from "./Subtitle";
-import ProductCard from "./ProductCard";
+import Title from "../components/Title";
+import Subtitle from "../components/Subtitle";
+import ProductCard from "../components/ProductCard";
 
 function Beans() {
   return (
@@ -9,13 +9,29 @@ function Beans() {
       <Title title="Coffee Beans" />
       <div id="product-wrapper">
         <Subtitle subtitle="Please browse our selection of coffee beans" />
-        <ProductCard
-          src="/Images/blaze.jpg"
-          alt="Picture of coffee beans called Blaze."
-          arialabel="More information on Blaze."
-          itemName="Blaze (200g)"
-          itemPrice="£11.99"
-        />
+        <div className="products">
+          <ProductCard
+            src="/Images/blaze.jpg"
+            alt="Picture of coffee beans called Blaze."
+            arialabel="More information on Blaze."
+            itemName="Blaze (200g)"
+            itemPrice="£11.99"
+          />
+          <ProductCard
+            src="/Images/sunshine.jpg"
+            alt="Picture of coffee beans called Sunshine."
+            arialabel="More information on Sunshine."
+            itemName="Sunshine (200g)"
+            itemPrice="£14.99"
+          />
+          <ProductCard
+            src="/Images/summit.jpg"
+            alt="Picture of coffee beans called Summit."
+            arialabel="More information on Summit."
+            itemName="Summit (150g)"
+            itemPrice="£19.99"
+          />
+        </div>
       </div>
     </>
   );
