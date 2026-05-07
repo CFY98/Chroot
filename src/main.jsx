bc09f84e6195a2502b049a1a3df27e840d36d23d
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 17:11:45 +0100

    feat: added StrictMode import to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 //IMPORTS
 import "./css/main.css";
 import ReactDom from "react-dom/client";
+import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDom.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
