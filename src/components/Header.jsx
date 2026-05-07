5cb0bbb7e718caddd92aae6c4c9695bff56f1a4e
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 23:04:57 +0100

    feat: added checkMode function to check tuiMode state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 //IMPORTS
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../css/Header.module.css";
 
 function Header() {
+  const [tuiMode, setTuiMode] = useState(false);
+  const navigate = useNavigate();
+
+  function checkMode() {
+    const newMode = !tuiMode;
+    setTuiMode(newMode);
+    navigate(newMode ? "/terminal" : "/");
+  }
+
   return (
     <div className={styles.titlebar}>
       <span className={`${styles.dot} ${styles["dot-r"]}`}></span>
@@ -9,8 +20,9 @@ function Header() {
       <span className={`${styles.dot} ${styles["dot-g"]}`}></span>
       <span
         className={styles["titlebar-text"]}
-        data-text="Visual Mode"
-        data-hover="Change: Terminal Mode"
+        data-text={tuiMode ? "Terminal Mode" : "Visual Mode"}
+        data-hover={tuiMode ? "Change: Visual Mode" : "Change: Terminal Mode"}
+        onClick={checkMode}
       ></span>
     </div>
   );
