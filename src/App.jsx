a127375a75804c00b6ec30c45b9cf3e073966585
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 18:00:30 +0100

    fix: imported transitions for use in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import Beans from "./pages/Beans";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { handleScroll } from "./tools/transitions";
 
 function App() {
+  useEffect(() => {
+    handleScroll();
+  }, []);
+
   return (
     <>
       <Header />
