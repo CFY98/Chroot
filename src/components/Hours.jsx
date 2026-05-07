6e4fa19cbe5d8fc37ff4a4e87d2d75b8e7488374
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 13:51:06 +0100

    feat: added hours component to Home

---
+++ src/components/Hours.jsx
@@ -0,0 +1,16 @@
+function Hours() {
+  return (
+    <div className="hours-container">
+      <div className="opening-container">
+        <h3 className="sub-heading fadein">Opening Hours</h3>
+        <p className="times fadein">
+          Mondays - Fridays: 07:00 → 18:00
+          <br />
+          Saturdays - Sundays: 08:00 → 16:00
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hours;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 //IMPORTS
 import Slideshow from "../components/Slideshow";
 import Story from "../components/Story";
+import Hours from "../components/Hours";
 
 function Home() {
   return (
     <>
       <Slideshow />
       <Story />
+      <Hours />
     </>
   );
 }
