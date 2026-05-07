4502f418af67f20a72b279ec6d31eea805dad297
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 15:00:22 +0100

    style: changed fragment to a window-container div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 //IMPORTS
+import "../css/gui.css";
 import Slideshow from "../components/Slideshow";
 import Story from "../components/Story";
 import Hours from "../components/Hours";
 
 function Home() {
   return (
-    <>
+    <div className="window-container">
       <Slideshow />
       <Story />
       <Hours />
-    </>
+    </div>
   );
 }
 
