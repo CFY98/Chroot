7acae98fca1e48bb8d9e21f4f5cac9e191b94a34
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-07 12:48:50 +0100

    chore: moved Captions, Navbar, ProductCard, Subtitle and Title to components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    <div className="nav-buttons">
+      <div className="nav-btn" id="title">
+        (Chroot)
+      </div>
+      <div className="nav-btn">Beans</div>
+      <div className="nav-btn">Equipment</div>
+      <div id="basket-btn" className="nav-btn">
+        Basket
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
