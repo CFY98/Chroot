ed1b71daf5e00660eb4f886704cf519073fd0e85
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-08 22:57:27 +0100

    feat: added Grinder components

---
+++ src/components/GrindFluff.jsx
@@ -0,0 +1,28 @@
+//IMPORTS
+import styles from "../css/Stats.module.css";
+
+function GrindFluff({ fluff, burrs, materials, weight, price }) {
+  return (
+    <div className={styles.statistics}>
+      <p className={styles.fluff}>{fluff}</p>
+      <span>
+        <br />
+        <br />
+      </span>
+      <p className={styles.stats}>
+        Burr Set: <span className={styles.result}>{burrs}</span>
+      </p>
+      <p className={styles.stats}>
+        Materials: <span className={styles.result}>{materials}</span>
+      </p>
+      <p className={styles.stats}>
+        Weight: <span className={styles.result}>{weight}</span>
+      </p>
+      <p className={styles.stats}>
+        Price: <span className={styles.result}>{price}</span>
+      </p>
+    </div>
+  );
+}
+
+export default GrindFluff;
---
+++ src/pages/Grinder.jsx
@@ -0,0 +1,57 @@
+//IMPORTS
+import styles from "../css/CatWrapper.module.css";
+import { useState } from "react";
+import Title from "../components/Title";
+import GrindFluff from "../components/GrindFluff";
+import Counter from "../components/Counter";
+import Toastbox from "../components/Toastbox";
+
+function Grinder() {
+  const [toast, setToast] = useState(null);
+  return (
+    <>
+      <Title title="Grinder" />
+      <div className={`${styles["catalogue-wrapper"]} fadein`}>
+        <div className={styles["catalogue-container"]}>
+          <div className={styles["catalogue-item"]}>
+            <img
+              src="/Images/grinder.jpg"
+              alt="Picture of the Chroot Grinder."
+            />
+          </div>
+          <GrindFluff
+            fluff={
+              <>
+                Embrace the morning with the Chroot Grinder.
+                <br />
+                <br />
+                Whether you're a fan of the French Press or need to dial-in
+                those espresso shots, the precision-made 48m conical burrs are
+                desinged for all needs from the enthusiast to the professional.
+                <br />
+                <br />
+                The compact size and weight of 600g allows for a sturdy yet
+                lightweight feel to provide a premium experience for the most
+                adventurous of drinkers.
+              </>
+            }
+            Burrs="48 Conical SS"
+            materials="7075 Alumninium"
+            Weight="600g"
+            price="£129.99"
+          />
+        </div>
+        <Counter itemName="grinder" setToast={setToast} />
+        {toast && (
+          <Toastbox
+            itemName={toast.itemName}
+            count={toast.count}
+            clearToast={() => setToast(null)}
+          />
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Grinder;
