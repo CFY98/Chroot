db60e6053ed458140dc14d62cc54870fcf7c7d62
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-08 19:06:02 +0100

    refactor: switched from ahref to NavLinks

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,17 @@
 //IMPORTS
 import styles from "../css/ProductCard.module.css";
+import { NavLink } from "react-router-dom";
 
-function ProductCard({ src, alt, arialabel, href, itemName, itemPrice }) {
+function ProductCard({ src, alt, arialabel, path, itemName, itemPrice }) {
   return (
     <>
       <div className={styles["caption-container"]}>
         <img src={src} alt={alt} className={`${styles.product} fadein`} />
-        <div className={styles.caption}>
-          <a
-            className={styles.more}
-            aria-label={arialabel}
-            href={href}
-            data-link
-          >
+        <NavLink className={styles.caption} to={path}>
+          <div className={styles.more} aria-label={arialabel} data-link>
             More info
-          </a>
-        </div>
+          </div>
+        </NavLink>
       </div>
       <div className={`${styles.information} fadein`}>
         <p className={styles.itemnames}>{itemName}</p>
