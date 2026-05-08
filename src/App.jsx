49c73cf591c86ee2f30706331d1df62427d2ba09
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-08 19:12:34 +0100

    chore: added Blaze component and tidied imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { handleScroll } from "./tools/transitions";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Beans from "./pages/Beans";
+import Blaze from "./pages/Blaze";
 import Equipment from "./pages/Equipment";
 import Basket from "./pages/Basket";
-import NavBar from "./components/NavBar";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
         <Route path="/beans" element={<Beans />} />
         <Route path="/equipment" element={<Equipment />} />
         <Route path="/basket" element={<Basket />} />
+        <Route path="/blaze" element={<Blaze />} />
       </Routes>
     </div>
       <Footer />
