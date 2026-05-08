3ffcce2de71bb19d7fb6fc9d795889a4d76b2fb8
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-08 21:36:06 +0100

    style: uppercase on itemname

--- src/pages/Blaze.jsx
+++ src/pages/Blaze.jsx
@@ -31,7 +31,7 @@ function Blaze() {
             price="£11.99"
           />
         </div>
-        <Counter itemName="blaze" setToast={setToast}/>
+        <Counter itemName="Blaze" setToast={setToast}/>
         {toast && <Toastbox itemName={toast.itemName} count={toast.count} clearToast={() => setToast(null)} />}
       </div>
     </>
