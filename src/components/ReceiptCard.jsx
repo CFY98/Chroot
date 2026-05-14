6542dad13e1a0f52f0dd1da356e595620bd536b9
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-14 13:54:43 +0100

    feat: ReceiptCard and style modules for Receipt.js

---
+++ src/components/ReceiptCard.jsx
@@ -0,0 +1,25 @@
+//IMPORTS
+import styles from "../css/ReceiptCard.module.css";
+import { productPrices, toTitleCase } from "../tools/assets.js";
+import { storage } from "../tools/storage.js";
+
+function ReceiptCard({ itemName }) {
+  const committed = storage.get("committed", {});
+  const qtyTotal = committed[itemName] * productPrices[itemName];
+  return (
+    <div className={styles["receipt-item"]}>
+      <div className={styles["image-box"]}>
+        <img src={`/Images/${itemName}.jpg`} alt={itemName} />
+      </div>
+      <div className={styles.about}>
+        <div className={styles["item-name"]}>{toTitleCase(itemName)}</div>
+      </div>
+      <div className={styles["item-count"]}>{committed[itemName]}</div>
+      <div className={styles["receipt-cost"]}>
+        <div className={styles["item-amount"]}>£{qtyTotal.toFixed(2)}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ReceiptCard;
--- src/pages/Receipt.jsx
+++ src/pages/Receipt.jsx
@@ -1,22 +1,55 @@
-function Receipt() {
+//IMPORTS
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import styles from "../css/Receipt.module.css";
+import { initReceipt } from "../purchase/receipt.js";
+import { announce } from "../tools/announcer.js";
+import { storage } from "../tools/storage.js";
+import Title from "../components/Title";
+import Subtitle from "../components/Subtitle";
+import ReceiptCard from "../components/ReceiptCard";
+
+function Receipt({ tuiMode }) {
+  useEffect(() => {
+    announce("Click the Download button to download or print the receipt");
+  }, []);
+
+  const navigate = useNavigate();
+  const committed = storage.get("committed", {});
+  const receiptItems = Object.keys(committed);
+
+  function receiptGen(tuiMode) {
+    if (initReceipt()) {
+      navigate(tuiMode ? "/terminal" : "/");
+    }
+  }
+
   return (
-    <div className={`{styles.invoice} fadein`}>
-      <p className={styles.subtitle}>Invoice</p>
-      <p className="thanks">Thank you for your purchase at Chroot!</p>
-      <div className="receipt-container">
-        <div className="bar">
-          <p className="order">Order Number:</p>
-          <p className="message"></p>
-          <button className="print" aria-label="print PDF">
+    <div className="fadein">
+      <Title title="Invoice" />
+      <Subtitle subtitle="Thank you for your purchase at Chroot!" />
+      <div className={styles["receipt-container"]}>
+        <div className={styles.bar}>
+          <p className={styles.order}>Order Number:</p>
+          <p className={styles.message}></p>
+          <button
+            className={styles.print}
+            aria-label="print PDF"
+            onClick={receiptGen}
+          >
             Download
           </button>
         </div>
-        <div className="receipt-items"></div>
+        <div className={styles["receipt-items"]}>
+          {receiptItems.map((key) => (
+            <ReceiptCard key={key} itemName={key} />
+          ))}
+        </div>
         <hr />
-        <div className="receipt-checkout">
-          <div className="receipt-total">
+        <div className={styles["receipt-checkout"]}>
+          <div className={styles["receipt-total"]}>
             Total
-            <div className="receipt-amount"></div>
+            <div className={styles["receipt-amount"]}></div>
           </div>
         </div>
       </div>
