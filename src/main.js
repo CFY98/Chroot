bca23f9920d6bc530aab55b8b8856a3607e698a6
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-05-15 12:35:33 +0100

    refactor: removed unused scripts

--- src/main.js
+++ src/main.js
@@ -1,9 +1,2 @@
 // IMPORTS
-import "./tools/routerSPA";
 import "./tools/transitions";
-import "./css/basket.css";
-import "./css/pages.css";
-import "./css/product.css";
-import "./css/receipt.css";
-import "./css/terminal.css";
-import "./css/pdf.css";
