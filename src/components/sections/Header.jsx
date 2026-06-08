35f48ac4523ee031ba1dcd439e776e7b08b6231b
cfy98 <232689414+CFY98@users.noreply.github.com>
2026-06-08 22:00:08 +0100

    refactor: changed span to button for improved accessibility

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -16,12 +16,13 @@ function Header({ tuiMode, setTuiMode }) {
 			<span className={`${styles.dot} ${styles["dot-r"]}`}></span>
 			<span className={`${styles.dot} ${styles["dot-y"]}`}></span>
 			<span className={`${styles.dot} ${styles["dot-g"]}`}></span>
-			<span
+			<button
+				type="button"
 				className={styles["titlebar-text"]}
 				data-text={tuiMode ? "Terminal Mode" : "Visual Mode"}
 				data-hover={tuiMode ? "Change: Visual Mode" : "Change: Terminal Mode"}
 				onClick={checkMode}
-			></span>
+			></button>
 		</div>
 	);
 }
